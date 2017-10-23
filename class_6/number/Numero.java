280ec799c73e308569bc1ceaec4634b83de36669
JoaoMiguelNeves <joaomcpneves@gmail.com>
2017-10-23 23:45:59 +0100

    Added javadoc comments

--- class_6/number/Numero.java
+++ class_6/number/Numero.java
@@ -1,29 +1,72 @@
 package number;
 
+/**
+ * Standart Numero Class. Stores an int number, which can be set or gotten,
+ * and can be compared with other Numeros.
+ * 
+ * @author Grupo 22
+ */
+
 class Numero extends Object {
 
+	/**
+	 * Numero's value.
+	 */
 	private int _value;
+
+	/**
+	 * Creates a default Numero with value set to 0.
+	 */
 
 	public Numero() {
 		_value = 0; 
 	}
 
+	/**
+	 * Creates a Numero with value set to val.
+	 *
+	 * @param val value of created Numero.
+	 */
+
 	public Numero( int val ) {
 		set(val);
 	}
 
+	/**
+	 * Returns Numero's value.
+	 *
+	 * @return Numero's value.
+	 */
 	public int get() {
 		return _value; 
 	}
+
+	/**
+	 * Sets Numero's value.
+	 *
+	 * @param val new value for Numero.
+	 */
 
 	public void set( int val ) {
 		_value = val;
 	}
 
+	/**
+	 * Returns a String which identifies the Numero.
+	 *
+	 * @return String identifier.
+	 */
+
 	public String toString() {
 		return "" + _value + " " + super.toString();
 	}
 
+	/**
+	 * Compares a Numero's value with another Numero, and checks if it's the same.
+	 *
+	 * @return logical value of comparison.
+	 */
+	
 	public boolean equals( Object other ) {
 		if ( other instanceof Numero == false ) {
 			return false;
--- class_6/number/NumeroComMemoria.java
+++ class_6/number/NumeroComMemoria.java
@@ -1,22 +1,55 @@
 package number;
 import number.Numero;
 
+/**
+ * Class which extends the standart Numero Class. Can revert previous set changes since it
+ * stores its last set number in memory. Its previous number can also be gotten.
+ *
+ * @author Grupo 22
+ */
+
 class NumeroComMemoria extends Numero {
 
     private int _prev;
 
+    /**
+     * Creates a number with memory with value val, and sets the previous value to val.
+     *
+     * @param val value of new number.
+     */
+
     public NumeroComMemoria ( int val ) {
         set(val);
     }
+
+    /**
+     * Sets the number's value to val, and leaves the number's previous value in memory.
+     * <p>
+     * The previous number stored in memory is lost.
+     * 
+     * @param val new value to be set.
+     */
 
     public void set ( int val ) {
         _prev = get();
         super.set(val);
     }
 
+    /**
+     * Gets the previous number stored in memory.
+     *
+     * @return the previous number's value.
+     */
+
     public int getPrev () {
         return _prev;
     }
+
+    /**
+     * Reverts the previous change to the number, and stores its current value in memory.
+     * <p>
+     * This can be reverted if the method is called again.
+     */
 
     public void undo () {
         set( getPrev() );
